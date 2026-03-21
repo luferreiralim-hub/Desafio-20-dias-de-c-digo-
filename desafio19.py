fcc884785e1adebedc8b5e71f48c4fe4fbc895d1
Luciana Ferreira <lu.ferreiralim@gmail.com>
2026-03-20 22:04:53 -0300

    Add files via upload

---
+++ desafio19.py
@@ -0,0 +1,10 @@
+N = int(input())
+
+print(N)
+
+notas = [100, 50, 20, 10, 5, 2, 1]
+
+for nota in notas:
+    quantidade = N // nota
+    print(f"{quantidade} nota(s) de R$ {nota},00")
+    N = N % nota
